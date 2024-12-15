76ee17aba0ee9cb723fd17a7ee18153ce37505c9
Vovamedvedev1 <mvamedvedev9@gmail.com>
2024-12-15 11:02:03 +0300

    New_res

--- s_p.cpp
+++ s_p.cpp
@@ -1,76 +1,137 @@
 ﻿#include <iostream>
 #include <fstream>
+#include <sstream>
 #include <iomanip>
 #include "mpi.h"
 using namespace std;
 
-void getResult(long long& count, int* nominals, int count_nominals, int sum, ofstream& file)
+// Функция для перебора комбинаций в заданном диапазоне
+void getResult(long long& count, int*& nominals, int*& structureNominals, int count_nominals, int sum, long long startCombination, long long endCombination, int rank)
 {
-    int* structureNominals = new int[count_nominals];
-    for (int i = 0; i < count_nominals; i++)
-        structureNominals[i] = 0;
+    string name = "example" + to_string(rank);
+    ofstream ofs(name);
+    if (!ofs.is_open())
+        throw "Error open file";
+    ofs << "ПРОЦЕСС " << rank << endl;
     int tempSum;
-    int k;
-    while (true)
-    {
+    long long tempCombination;
+    for (long long currentCombination = startCombination; currentCombination < endCombination; currentCombination++) {
+        tempCombination = currentCombination;
         tempSum = 0;
         for (int i = 0; i < count_nominals; i++)
+        {
+            structureNominals[i] = tempCombination % (sum / nominals[i] + 1);
+            tempCombination /= (sum / nominals[i] + 1);
             tempSum += nominals[i] * structureNominals[i];
+        }
         if (tempSum == sum)
         {
             count++;
-            file << "Решение " << count << ": ";
+            ofs << "Решение " << count << ": ";
             for (int i = 0; i < count_nominals; ++i)
-                file << nominals[i] << ":" << structureNominals[i] << " ";
-            file << endl;
+                ofs << nominals[i] << ":" << structureNominals[i] << " ";
+            ofs << endl;
         }
-        k = count_nominals - 1;
-        while (k >= 0 && structureNominals[k] == sum / nominals[k])
-        {
-            structureNominals[k] = 0;
-            k--;
-        }
-        if (k < 0)
-            break;
-        structureNominals[k]++;
     }
+    ofs.close();
 }
 
-
-int main(int argc, char** argv)
-{
+int main(int argc, char** argv) {
     setlocale(LC_ALL, "Russian");
     int rank, size;
     double start, stop;
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    int sum;   //искомая сумма
-    cout << "Введите сумму, которую требуется набрать: "; cin >> sum;
-    int count_nominals;   //количество номиналов монет
-    cout << "Введите количество номиналов монет: "; cin >> count_nominals;
-    int* nominals = new int[count_nominals];   //номиналы монет
-    cout << "Введите номиналы монет в рублях: ";
-    for (int i = 0; i < count_nominals; i++)
-        cin >> nominals[i];
-    ofstream file("result.txt");
-    if (file.is_open())
-    {
-        long long count = 0;
+    int sum;
+    int count_nominals;
+    int* re = new int[2];
+    int* nominals = nullptr;
+    if (rank == 0) {
         start = MPI_Wtime();
-        getResult(count, nominals, count_nominals, sum, file);
-        stop = MPI_Wtime();
-        cout << "-------- Количество вариантов набрать 100 руб: " << count << "----------" << endl;
-        cout << setprecision(10) << fixed << "Время, затраченное на перебор: " << fixed << stop - start << endl;
-    }
-    else
+        cout << "Enter the amount you want to dial: "; cin >> sum;
+        cout << "Enter the number of coin denominations: "; cin >> count_nominals;
+        nominals = new int[count_nominals];
+        cout << "Enter the coin denominations in rubles: ";
+        for (int i = 0; i < count_nominals; i++) {
+            cin >> nominals[i];
+        }
+        re[0] = sum;
+        re[1] = count_nominals;
+    } 
+    MPI_Bcast(&re[0], 2, MPI_INT, 0, MPI_COMM_WORLD);
+    sum = re[0];
+    count_nominals = re[1];
+    delete[]re;
+    int* structureNominals = new int[count_nominals];
+    for (int i = 0; i < count_nominals; i++)
+        structureNominals[i] = 0;
+    if (rank != 0)
+        nominals = new int[count_nominals];
+    MPI_Bcast(nominals, count_nominals, MPI_INT, 0, MPI_COMM_WORLD);
+    long long totalCombinations = 1;
+    for (int i = 0; i < count_nominals; ++i)
+        totalCombinations *= (sum / nominals[i] + 1);
+    long long cS = totalCombinations / size;
+    long long startCombination = rank * cS;
+    long long endCombination = (rank == size - 1) ? totalCombinations : startCombination + cS;
+    long long localCount = 0;
+    long long globalCount = 0;
+    try
     {
-        cout << "Ошибка открытия файла!!!" << endl;
+        getResult(localCount, nominals, structureNominals, count_nominals, sum, startCombination, endCombination, rank);
+    }
+    catch (const char* ex)
+    {
+        std::cout << ex << endl;
+        MPI_Abort(MPI_COMM_WORLD, 0);
         return 0;
     }
-    file.close();
-    cout << "Результат записан !!!" << endl;
-    delete[]nominals;
+
+    MPI_Reduce(&localCount, &globalCount, 1, MPI_LONG, MPI_SUM, 0, MPI_COMM_WORLD);
+    if (rank == 0)
+    {
+        ofstream finalFile("final_output.txt");
+        if (finalFile.is_open())
+        {
+            for (int i = 0; i < size; i++) {
+                string name = "example" + to_string(i);
+                ifstream localFile(name);
+                if (localFile.is_open())
+                {
+                    string line;
+                    while (getline(localFile, line))
+                        finalFile << line << endl;
+                    localFile.close();
+                    if (remove(name.c_str()) != 0)
+                    {
+                        cout << "Ошибка при удалении файла " << name << endl;
+                        MPI_Abort(MPI_COMM_WORLD, 0);
+                        return 0;
+                    }
+                }
+                else
+                {
+                    cout << "Ошибка при открытии файла " << name << endl;
+                    MPI_Abort(MPI_COMM_WORLD, 0);
+                    return 0;
+                }
+            }
+            finalFile.close();
+            cout << "Все файлы объединены в final_output.txt" << endl;
+        }
+        else
+        {
+            cout << "Ошибка при открытии файла final_output.txt" << endl;
+            MPI_Abort(MPI_COMM_WORLD, 0);
+            return 0;
+        }
+        stop = MPI_Wtime();
+        cout << "-------- The total number of options to dial " << sum << " руб: " << globalCount << "----------" << endl;
+        cout << setprecision(10) << fixed << "TIME: " << fixed << stop - start << endl;
+    }
+    delete[] nominals;
+    delete[] structureNominals;
     MPI_Finalize();
     return 0;
 }
